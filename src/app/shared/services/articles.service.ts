1777b2d7f92cbc88fd506ffe6d0cc43de17d459e
kazacoder <avtk.tlt@gmail.com>
2025-03-27 09:57:07 +0300

    Filtering realized on the Blog page.

--- src/app/shared/services/articles.service.ts
+++ src/app/shared/services/articles.service.ts
@@ -4,6 +4,8 @@ import {ArticleType} from "../../../types/article.type";
 import {Observable} from "rxjs";
 import {DefaultResponseType} from "../../../types/default-response.type";
 import {environment} from "../../../environments/environment";
+import {CategoryType} from "../../../types/category.type";
+import {ActiveParamsType} from "../../../types/active-params.type";
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +18,13 @@ export class ArticlesService {
     return this.http.get<ArticleType[] | DefaultResponseType>(environment.api + 'articles/top')
   }
 
-  getArticles(): Observable<{ count: number, pages: number, items: ArticleType[] }> {
-    return this.http.get<{ count: number, pages: number, items: ArticleType[] }>(environment.api + 'articles')
+  getArticles(params: ActiveParamsType): Observable<{ count: number, pages: number, items: ArticleType[] }> {
+    return this.http.get<{ count: number, pages: number, items: ArticleType[] }>(environment.api + 'articles', {
+      params: params
+    })
+  }
+
+  getCategories(): Observable<CategoryType[]> {
+    return this.http.get<CategoryType[]>(environment.api + 'categories')
   }
 }
--- src/app/views/article/blog/blog.component.ts
+++ src/app/views/article/blog/blog.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ArticlesService} from "../../../shared/services/articles.service";
 import {ArticleType} from "../../../../types/article.type";
+import {CategoryType} from "../../../../types/category.type";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-blog',
@@ -10,14 +12,33 @@ import {ArticleType} from "../../../../types/article.type";
 export class BlogComponent implements OnInit {
 
   filterOpened: boolean = false;
-
+  categories: CategoryType[] = [];
+  categoriesFilterList: CategoryType[] = [];
   articles: ArticleType[] = [];
-  constructor(private articleService: ArticlesService) { }
+
+  constructor(private articleService: ArticlesService) {
+  }
 
   ngOnInit(): void {
-    this.articleService.getArticles().subscribe(data => {
-      this.articles = data.items
-      console.log(this.articles)
+    this.articleService.getArticles({categories: []}).subscribe({
+      next: data => {
+        this.articles = data.items
+      },
+      error: (err: HttpErrorResponse) => {
+      console.log(err.message)
+    }
+    })
+
+    this.articleService.getCategories().subscribe({
+      next: data => {
+        this.categories = data.map(item => {
+          item.filterOn = false;
+          return item;
+        })
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err.message)
+      }
     })
   }
 
@@ -25,4 +46,24 @@ export class BlogComponent implements OnInit {
     this.filterOpened = !this.filterOpened;
   }
 
+  applyFilter(categoryId: string): void {
+    const currentCategory = this.categories.find((category) => category.id === categoryId);
+    if (currentCategory) {
+      if (currentCategory.filterOn) {
+        currentCategory.filterOn = false;
+        this.categoriesFilterList = this.categoriesFilterList.filter(category => category.id !== currentCategory.id);
+      } else {
+        currentCategory.filterOn = true;
+        this.categoriesFilterList.push(currentCategory);
+      }
+    }
+    this.articleService.getArticles({categories: this.categoriesFilterList.map(item => item.url)}).subscribe({
+      next: data => {
+        this.articles = data.items
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err.message)
+      }
+    })
+  }
 }
