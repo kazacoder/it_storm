96e80504a2b25e70a40d52a702f26db796079f96
kazacoder <avtk.tlt@gmail.com>
2025-03-27 18:00:55 +0300

    Filtering Blog page is in progress.

--- src/app/views/article/blog/blog.component.ts
+++ src/app/views/article/blog/blog.component.ts
@@ -3,6 +3,8 @@ import {ArticlesService} from "../../../shared/services/articles.service";
 import {ArticleType} from "../../../../types/article.type";
 import {CategoryType} from "../../../../types/category.type";
 import {HttpErrorResponse} from "@angular/common/http";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ActiveParamsType} from "../../../../types/active-params.type";
 
 @Component({
   selector: 'app-blog',
@@ -15,55 +17,99 @@ export class BlogComponent implements OnInit {
   categories: CategoryType[] = [];
   categoriesFilterList: CategoryType[] = [];
   articles: ArticleType[] = [];
+  activeParams: ActiveParamsType = {categories: []}
 
-  constructor(private articleService: ArticlesService) {
+  constructor(private articleService: ArticlesService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router,) {
   }
 
   ngOnInit(): void {
-    this.articleService.getArticles({categories: []}).subscribe({
-      next: data => {
-        this.articles = data.items
-      },
-      error: (err: HttpErrorResponse) => {
-      console.log(err.message)
-    }
-    })
-
     this.articleService.getCategories().subscribe({
       next: data => {
         this.categories = data.map(item => {
           item.filterOn = false;
           return item;
-        })
+        });
+        this.proceedArticles();
       },
       error: (err: HttpErrorResponse) => {
-        console.log(err.message)
+        console.log(err.message);
+        this.proceedArticles();
       }
-    })
+    });
+
   }
 
   toggleFilter() {
     this.filterOpened = !this.filterOpened;
   }
 
-  applyFilter(categoryId: string): void {
+  applyFilter(categoryId: string, filterOn?: boolean): void {
     const currentCategory = this.categories.find((category) => category.id === categoryId);
+
+    if (this.activeParams.categories && this.activeParams.categories.length > 0) {
+
+    }
+
     if (currentCategory) {
       if (currentCategory.filterOn) {
         currentCategory.filterOn = false;
-        this.categoriesFilterList = this.categoriesFilterList.filter(category => category.id !== currentCategory.id);
+        // this.categoriesFilterList = this.categoriesFilterList.filter(category => category.id !== currentCategory.id);
+        this.activeParams.categories = this.activeParams.categories.filter(category => category !== currentCategory.url);
+
       } else {
         currentCategory.filterOn = true;
-        this.categoriesFilterList.push(currentCategory);
+        // this.categoriesFilterList.push(currentCategory);
+        this.activeParams.categories.push(currentCategory.url);
+        // this.activeParams.categories = []
+        // this.activeParams.page = 1
+        console.log('here1')
       }
     }
-    this.articleService.getArticles({categories: this.categoriesFilterList.map(item => item.url)}).subscribe({
-      next: data => {
-        this.articles = data.items
-      },
-      error: (err: HttpErrorResponse) => {
-        console.log(err.message)
+
+    this.router.navigate(['/articles'], {queryParams: this.activeParams}).then();
+    // this.proceedArticles()
+
+    console.log(this.categories)
+    console.log(currentCategory)
+    console.log(this.activeParams.categories)
+
+  }
+
+  proceedArticles() {
+    this.activatedRoute.queryParams.subscribe(params => {
+      console.log('here2')
+      if (params.hasOwnProperty("categories")) {
+        this.categoriesFilterList = [];
+
+        this.activeParams.categories = Array.isArray(params['categories']) ? params['categories'] : [params['categories']];
+        this.activeParams.categories.forEach((categoryUrl) => {
+          const currentCategory = this.categories.find((category) => category.url === categoryUrl);
+          console.log(categoryUrl);
+          if (currentCategory) {
+            this.categoriesFilterList.push(currentCategory);
+            currentCategory.filterOn = true;
+          }
+
+        });
       }
-    })
+
+      if (params.hasOwnProperty('page')) {
+        this.activeParams.page = +params['page'];
+      }
+
+      this.articleService.getArticles(this.activeParams).subscribe({
+        next: data => {
+          this.articles = data.items;
+        },
+        error: (err: HttpErrorResponse) => {
+          console.log(err.message);
+        }
+      });
+
+      // this.router.navigate(['/articles'], {queryParams: this.activeParams}).then();
+
+    });
   }
 }
