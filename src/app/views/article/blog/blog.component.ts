6a10709458f5a689d351f48bfc8376239ae1c6c2
user <user@test.net>
2025-03-27 22:59:07 +0300

    Filtering on the Blog page is realized through queryParams

--- src/app/views/article/blog/blog.component.ts
+++ src/app/views/article/blog/blog.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, HostListener, OnInit} from '@angular/core';
 import {ArticlesService} from "../../../shared/services/articles.service";
 import {ArticleType} from "../../../../types/article.type";
 import {CategoryType} from "../../../../types/category.type";
@@ -9,19 +9,29 @@ import {ActiveParamsType} from "../../../../types/active-params.type";
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  styleUrls: ['./blog.component.scss'],
 })
-export class BlogComponent implements OnInit {
+
+
+
+export class BlogComponent implements OnInit, AfterViewInit {
 
   filterOpened: boolean = false;
   categories: CategoryType[] = [];
   categoriesFilterList: CategoryType[] = [];
   articles: ArticleType[] = [];
   activeParams: ActiveParamsType = {categories: []}
+  filterDropDown: Element | null = null;
 
   constructor(private articleService: ArticlesService,
               private activatedRoute: ActivatedRoute,
               private router: Router,) {
+  }
+
+  ngAfterViewInit() {
+    // ToDo
+    // doesn't work???
+    this.filterDropDown = document.querySelector('blog-filters')
   }
 
   ngOnInit(): void {
@@ -41,57 +51,50 @@ export class BlogComponent implements OnInit {
 
   }
 
+  @HostListener('document:click', ['$event'])
+  onClick(event: Event): void {
+    if (!this.filterDropDown) {
+      this.filterDropDown = document.querySelector('.blog-filters')
+    }
+
+    if (!document.querySelector('.blog-filters')?.contains(event.target as Node)) {
+      this.filterOpened = false;
+    }
+  }
+
+
   toggleFilter() {
     this.filterOpened = !this.filterOpened;
   }
 
-  applyFilter(categoryId: string, filterOn?: boolean): void {
+  applyFilter(categoryId: string): void {
     const currentCategory = this.categories.find((category) => category.id === categoryId);
-
-    if (this.activeParams.categories && this.activeParams.categories.length > 0) {
-
-    }
 
     if (currentCategory) {
       if (currentCategory.filterOn) {
         currentCategory.filterOn = false;
-        // this.categoriesFilterList = this.categoriesFilterList.filter(category => category.id !== currentCategory.id);
         this.activeParams.categories = this.activeParams.categories.filter(category => category !== currentCategory.url);
 
       } else {
         currentCategory.filterOn = true;
-        // this.categoriesFilterList.push(currentCategory);
-        this.activeParams.categories.push(currentCategory.url);
-        // this.activeParams.categories = []
-        // this.activeParams.page = 1
-        console.log('here1')
+        this.activeParams.categories = [...this.activeParams.categories, currentCategory.url];
       }
     }
 
-    this.router.navigate(['/articles'], {queryParams: this.activeParams}).then();
-    // this.proceedArticles()
-
-    console.log(this.categories)
-    console.log(currentCategory)
-    console.log(this.activeParams.categories)
-
+    this.router.navigate([], { relativeTo: this.activatedRoute, queryParams: this.activeParams }).then();
   }
 
   proceedArticles() {
     this.activatedRoute.queryParams.subscribe(params => {
-      console.log('here2')
       if (params.hasOwnProperty("categories")) {
         this.categoriesFilterList = [];
-
         this.activeParams.categories = Array.isArray(params['categories']) ? params['categories'] : [params['categories']];
         this.activeParams.categories.forEach((categoryUrl) => {
           const currentCategory = this.categories.find((category) => category.url === categoryUrl);
-          console.log(categoryUrl);
           if (currentCategory) {
             this.categoriesFilterList.push(currentCategory);
             currentCategory.filterOn = true;
           }
-
         });
       }
 
@@ -107,9 +110,6 @@ export class BlogComponent implements OnInit {
           console.log(err.message);
         }
       });
-
-      // this.router.navigate(['/articles'], {queryParams: this.activeParams}).then();
-
     });
   }
 }
